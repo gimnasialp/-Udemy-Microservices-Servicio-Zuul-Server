52fafe577bc07aeabe5dc14ca6e2828f96cb172a
migue_centellas_leon <migue.centellas@gmail.com>
2021-08-01 20:27:17 -0300

    seccion 6 - servicio commons - @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}) -- @EntityScan({"com.formacionbdi.springboot.app.commons.models.entity"}) -- mvnw.cmd install

---
+++ springboot-servicio-commons/src/main/java/com/formacionbdi/springboot/app/commons/models/entity/Producto.java
@@ -0,0 +1,64 @@
+package com.formacionbdi.springboot.app.commons.models.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name="productos")
+public class Producto implements Serializable {  //para convertir en bits, sirve para guardar en sesion, en archivo de texto, es funcional
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)   //autoincremental, identity identidad autoincrementada
+    private Long id;
+
+    private String nombre;
+    private Double precio;
+
+    @Column(name="create_at")
+    @Temporal(TemporalType.DATE)  //tipo de tiempo (date, fecha año mes dia)
+    private Date createAt;
+
+    @Transient
+    private Integer port;
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+}
